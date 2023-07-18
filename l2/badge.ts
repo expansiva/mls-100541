83b5bddc6eb931b6aa4876d09bd9d00da06e33c7
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-07-18 16:55:10 -0300

    Save mls file:l2/badge.ts

---
+++ l2/badge.ts
@@ -0,0 +1,45 @@
+/// <mls shortName="badge" project="100541" enhancement="_100541_enhancementLit" groupName="status" />
+
+import { customElement, property } from 'lit/decorators.js';
+import { html, LitElement, classMap } from 'lit';
+
+@customElement('badge-100541')
+export default class Badge extends LitElement {
+
+  /** The badge's theme variant. */
+  @property({ reflect: true }) variant: 'primary' | 'success'  | 'warning' | 'danger' = 'primary';
+
+  /** Draws a pill-style badge with rounded edges. */
+  @property({ type: Boolean, reflect: true }) pill = false;
+
+  /** Makes the badge pulsate to draw attention. */
+  @property({ type: Boolean, reflect: true }) pulse = false;
+
+  /** The badge's text. */
+  @property({ type: String, reflect: true }) text = '';
+
+  createRenderRoot() {
+    return this;
+  }
+
+  render() {
+
+    return html`
+      <span
+        part="base"
+        class=${classMap({
+          badge: true,
+          'badge--primary': this.variant === 'primary',
+          'badge--success': this.variant === 'success',
+          'badge--warning': this.variant === 'warning',
+          'badge--danger': this.variant === 'danger',
+          'badge--pill': this.pill,
+          'badge--pulse': this.pulse
+        })}
+        role="status"
+      >
+      ${this.text}
+      </span>
+    `;
+  }
+}
