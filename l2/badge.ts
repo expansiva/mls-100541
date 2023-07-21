a4e3da297133910247c393fc09bbf04236cc3bb9
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-07-21 17:27:26 -0300

    Save mls file:l2/badge.ts

--- l2/badge.ts
+++ l2/badge.ts
@@ -1,5 +1,9 @@
 /// <mls shortName="badge" project="100541" enhancement="_100541_enhancementLit" groupName="status" />
 
+/**
+ * This code has been forked and modified from a project found on https://github.com/shoelace-style/shoelace.
+ * The original project is licensed under the MIT license.
+ */
 import { customElement, property } from 'lit/decorators.js';
 import { html, LitElement, classMap } from 'lit';
 
