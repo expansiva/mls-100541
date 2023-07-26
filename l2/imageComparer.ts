d02bebcb682c09c98aa583d09986f29e6748813e
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-07-26 10:20:00 -0300

    Save mls file:l2/imageComparer.ts

--- l2/imageComparer.ts
+++ l2/imageComparer.ts
@@ -3,6 +3,7 @@
 /**
  * This code has been forked and modified from a project found on https://github.com/shoelace-style/shoelace.
  * The original project is licensed under the MIT license.
+ * @mlsComponentDetails {"webComponentDependencies": ["icon-100541"]}
  */
 
 import { html, classMap, styleMap, LitElement } from 'lit';
@@ -23,123 +24,122 @@ import { watch } from './_100541_internalWatch';
  *
  * @cssproperty --divider-width - The width of the dividing line.
  * @cssproperty --handle-size - The size of the compare handle.
- * @componentDetails {"dependencies": ["icon-100541"]}
  */
 @customElement('image-comparer-100541')
 export class ImageComparer extends LitElement {
-    @query('.image-comparer') base: HTMLElement;
-    @query('.image-comparer__handle') handle: HTMLElement;
+  @query('.image-comparer') base: HTMLElement;
+  @query('.image-comparer__handle') handle: HTMLElement;
 
-    /**
-     *  The position of the divider as a percentage.
-     * @fieldType { "type":"number", "defaultValue":"50" "max": "100", "min":"0"}
-     */
-    @property({ type: Number, reflect: true }) position = 50;
+  /**
+   *  The position of the divider as a percentage.
+   * @fieldType { "type":"number", "defaultValue":"50" "max": "100", "min":"0"}
+   */
+  @property({ type: Number, reflect: true }) position = 50;
 
-    /** The path to the image before to load. */
-    @property() srcBefore: string;
+  /** The path to the image before to load. */
+  @property() srcBefore: string;
 
-    /** The path to the image after to load. */
-    @property() srcAfter: string;
+  /** The path to the image after to load. */
+  @property() srcAfter: string;
 
-    private clamp(value: number, min: number, max: number) {
-        const noNegativeZero = (n: number) => (Object.is(n, -0) ? 0 : n);
-        if (value < min) return noNegativeZero(min);
-        if (value > max) return noNegativeZero(max);
-        return noNegativeZero(value);
+  private clamp(value: number, min: number, max: number) {
+    const noNegativeZero = (n: number) => (Object.is(n, -0) ? 0 : n);
+    if (value < min) return noNegativeZero(min);
+    if (value > max) return noNegativeZero(max);
+    return noNegativeZero(value);
+  }
+
+  private drag(container: HTMLElement, options?: any) {
+    function move(pointerEvent: PointerEvent) {
+      const dims = container.getBoundingClientRect();
+      const defaultView = container.ownerDocument.defaultView!;
+      const offsetX = dims.left + defaultView.pageXOffset;
+      const offsetY = dims.top + defaultView.pageYOffset;
+      const x = pointerEvent.pageX - offsetX;
+      const y = pointerEvent.pageY - offsetY;
+
+      if (options?.onMove) {
+        options.onMove(x, y);
+      }
     }
 
-    private drag(container: HTMLElement, options?: any) {
-        function move(pointerEvent: PointerEvent) {
-            const dims = container.getBoundingClientRect();
-            const defaultView = container.ownerDocument.defaultView!;
-            const offsetX = dims.left + defaultView.pageXOffset;
-            const offsetY = dims.top + defaultView.pageYOffset;
-            const x = pointerEvent.pageX - offsetX;
-            const y = pointerEvent.pageY - offsetY;
+    function stop() {
+      document.removeEventListener('pointermove', move);
+      document.removeEventListener('pointerup', stop);
 
-            if (options?.onMove) {
-                options.onMove(x, y);
-            }
-        }
-
-        function stop() {
-            document.removeEventListener('pointermove', move);
-            document.removeEventListener('pointerup', stop);
-
-            if (options?.onStop) {
-                options.onStop();
-            }
-        }
-
-        document.addEventListener('pointermove', move, { passive: true });
-        document.addEventListener('pointerup', stop);
-
-        // If an initial event is set, trigger the first drag immediately
-        if (options?.initialEvent instanceof PointerEvent) {
-            move(options.initialEvent);
-        }
+      if (options?.onStop) {
+        options.onStop();
+      }
     }
 
-    private handleDrag(event: PointerEvent) {
-        const { width } = this.base.getBoundingClientRect();
-        const isRtl = true; //this.localize.dir() === 'rtl';
+    document.addEventListener('pointermove', move, { passive: true });
+    document.addEventListener('pointerup', stop);
 
-        event.preventDefault();
-
-        this.drag(this.base, {
-            onMove: x => {
-                this.position = parseFloat(this.clamp((x / width) * 100, 0, 100).toFixed(2));
-                if (isRtl) this.position = 100 - this.position;
-            },
-            initialEvent: event
-        });
+    // If an initial event is set, trigger the first drag immediately
+    if (options?.initialEvent instanceof PointerEvent) {
+      move(options.initialEvent);
     }
+  }
 
-    private handleKeyDown(event: KeyboardEvent) {
-        const isLtr = true; // this.localize.dir() === 'ltr';
-        const isRtl = true;//  this.localize.dir() === 'rtl';
+  private handleDrag(event: PointerEvent) {
+    const { width } = this.base.getBoundingClientRect();
+    const isRtl = true; //this.localize.dir() === 'rtl';
 
-        if (['ArrowLeft', 'ArrowRight', 'Home', 'End'].includes(event.key)) {
-            const incr = event.shiftKey ? 10 : 1;
-            let newPosition = this.position;
+    event.preventDefault();
 
-            event.preventDefault();
+    this.drag(this.base, {
+      onMove: (x: any) => {
+        this.position = parseFloat(this.clamp((x / width) * 100, 0, 100).toFixed(2));
+        if (isRtl) this.position = 100 - this.position;
+      },
+      initialEvent: event
+    });
+  }
 
-            if ((isLtr && event.key === 'ArrowLeft') || (isRtl && event.key === 'ArrowRight')) {
-                newPosition -= incr;
-            }
-            if ((isLtr && event.key === 'ArrowRight') || (isRtl && event.key === 'ArrowLeft')) {
-                newPosition += incr;
-            }
-            if (event.key === 'Home') {
-                newPosition = 0;
-            }
-            if (event.key === 'End') {
-                newPosition = 100;
-            }
-            newPosition = this.clamp(newPosition, 0, 100);
+  private handleKeyDown(event: KeyboardEvent) {
+    const isLtr = true; // this.localize.dir() === 'ltr';
+    const isRtl = true;//  this.localize.dir() === 'rtl';
 
-            this.position = newPosition;
-        }
+    if (['ArrowLeft', 'ArrowRight', 'Home', 'End'].includes(event.key)) {
+      const incr = event.shiftKey ? 10 : 1;
+      let newPosition = this.position;
+
+      event.preventDefault();
+
+      if ((isLtr && event.key === 'ArrowLeft') || (isRtl && event.key === 'ArrowRight')) {
+        newPosition -= incr;
+      }
+      if ((isLtr && event.key === 'ArrowRight') || (isRtl && event.key === 'ArrowLeft')) {
+        newPosition += incr;
+      }
+      if (event.key === 'Home') {
+        newPosition = 0;
+      }
+      if (event.key === 'End') {
+        newPosition = 100;
+      }
+      newPosition = this.clamp(newPosition, 0, 100);
+
+      this.position = newPosition;
     }
+  }
 
-    createRenderRoot() {
-        return this;
-    }
+  createRenderRoot() {
+    return this;
+  }
 
-    @watch('position', { waitUntilFirstUpdate: true })
-    render() {
-        const isRtl = true; // this.localize.dir() === 'rtl';
+  @watch('position', { waitUntilFirstUpdate: true })
+  render() {
+    const isRtl = true; // this.localize.dir() === 'rtl';
 
-        return html`
+    return html`
       <div
         part="base"
         id="image-comparer"
         class=${classMap({
-            'image-comparer': true,
-            'image-comparer--rtl': isRtl
-        })}
+      'image-comparer': true,
+      'image-comparer--rtl': isRtl
+    })}
         @keydown=${this.handleKeyDown}
       >
         <div class="image-comparer__image">
@@ -151,8 +151,8 @@ export class ImageComparer extends LitElement {
             part="after"
             class="image-comparer__after"
             style=${styleMap({
-            clipPath: isRtl ? `inset(0 0 0 ${100 - this.position}%)` : `inset(0 ${100 - this.position}% 0 0)`
-        })}
+      clipPath: isRtl ? `inset(0 0 0 ${100 - this.position}%)` : `inset(0 ${100 - this.position}% 0 0)`
+    })}
           >
             <img src="${this.srcAfter}"></img>
           </div>
@@ -162,8 +162,8 @@ export class ImageComparer extends LitElement {
           part="divider"
           class="image-comparer__divider"
           style=${styleMap({
-            left: isRtl ? `${100 - this.position}%` : `${this.position}%`
-        })}
+      left: isRtl ? `${100 - this.position}%` : `${this.position}%`
+    })}
           @mousedown=${this.handleDrag}
           @touchstart=${this.handleDrag}
         >
@@ -182,7 +182,7 @@ export class ImageComparer extends LitElement {
         </div>
       </div>
     `;
-    }
+  }
 }
 
 declare global {
