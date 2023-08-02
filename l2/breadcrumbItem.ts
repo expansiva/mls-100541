dae4d81e8a30b14c6dc6bd619614598bba7e1ac0
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-08-02 17:07:51 -0300

    Save mls file:l2/breadcrumbItem.ts

--- l2/breadcrumbItem.ts
+++ l2/breadcrumbItem.ts
@@ -3,6 +3,7 @@
 /**
  * This code has been forked and modified from a project found on https://github.com/shoelace-style/shoelace.
  * The original project is licensed under the MIT license.
+ * @mlsComponentDetails {"webComponentDependencies": ["icon-100541"]}
  */
 
 import { html, ifDefined, classMap, LitElement } from 'lit';
@@ -11,50 +12,41 @@ import { customElement, property } from 'lit/decorators.js';
 @customElement('breadcrumb-item-100541')
 export class BreadcrumbItem extends LitElement {
 
-    /**
-    * An optional prefix, usually an icon or icon button.
-    */
-    @property() prefix: string;
+  /**
+  * A text for item
+  */
+  @property({ type: String, reflect: true }) text = '';
 
-    /**
-     *An optional suffix, usually an icon or icon button.
-     */
-    @property() sufix: string;
 
-    /**
-     * Optional URL to direct the user to when the breadcrumb item is activated. When set, a link will be rendered
-     * internally. When unset, a button will be rendered instead.
-     */
-    @property() href?: string;
+  /**
+   * Optional URL to direct the user to when the breadcrumb item is activated. When set, a link will be rendered
+   * internally. When unset, a button will be rendered instead.
+   */
+  @property() href?: string;
 
-    /** Tells the browser where to open the link. Only used when `href` is set. */
-    @property() target?: '_blank' | '_parent' | '_self' | '_top';
+  /** Tells the browser where to open the link. Only used when `href` is set. */
+  @property() target?: '_blank' | '_parent' | '_self' | '_top';
 
-    /** The `rel` attribute to use on the link. Only used when `href` is set. */
-    @property() rel = 'noreferrer noopener';
+  /** The `rel` attribute to use on the link. Only used when `href` is set. */
+  @property() rel = 'noreferrer noopener';
 
-    createRenderRoot() {
-        return this;
-    }
+  createRenderRoot() {
+    return this;
+  }
 
-    render() {
-        const isLink = this.href ? true : false;
+  render() {
+    const isLink = this.href ? true : false;
 
-        return html`
+    return html`
       <div
         part="base"
         class=${classMap({
-            'breadcrumb-item': true,
-            'breadcrumb-item--has-prefix': !!this.prefix,
-            'breadcrumb-item--has-suffix': !!this.sufix,
-        })}
-      >
-        <span part="prefix" class="breadcrumb-item__prefix">
-          <slot name="prefix"></slot>
-        </span>
+      'breadcrumb-item': true,
 
+    })}
+      >
         ${isLink
-                ? html`
+        ? html`
               <a
                 part="label"
                 class="breadcrumb-item__label breadcrumb-item__label--link"
@@ -62,29 +54,27 @@ export class BreadcrumbItem extends LitElement {
                 target="${ifDefined(this.target ? this.target : undefined)}"
                 rel=${ifDefined(this.target ? this.rel : undefined)}
               >
-                <slot></slot>
+                <slot>${this.text}</slot>
               </a>
             `
-                : html`
+        : html`
               <button part="label" type="button" class="breadcrumb-item__label breadcrumb-item__label--button">
-                <slot></slot>
+                <slot>${this.text}</slot>
               </button>
             `}
 
-        <span part="suffix" class="breadcrumb-item__suffix">
-          <slot name="suffix"></slot>
-        </span>
-
         <span part="separator" class="breadcrumb-item__separator" aria-hidden="true">
-          <slot name="separator"></slot>
+          <slot name="separator">
+            <icon-100541 name='chevron-right' library="default"></icon-100541>
+          </slot>
         </span>
       </div>
     `;
-    }
+  }
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'breadcrumb-item-100541': BreadcrumbItem;
-    }
+  interface HTMLElementTagNameMap {
+    'breadcrumb-item-100541': BreadcrumbItem;
+  }
 }
