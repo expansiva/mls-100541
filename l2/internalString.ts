542f5743e9609108be9715977df45a564f056e4b
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-08-04 08:53:20 -0300

    Save mls file:l2/internalString.ts

---
+++ l2/internalString.ts
@@ -0,0 +1,6 @@
+/// <mls shortName="internalString" project="100541" enhancement="_blank" />
+				
+/** Converts the first letter of a string to uppercase */
+export function uppercaseFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
