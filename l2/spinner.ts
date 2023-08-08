d81d24c79a91dd12da6378e3f68af58da80552e3
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-08-08 14:38:37 -0300

    Save mls file:l2/spinner.ts

--- l2/spinner.ts
+++ l2/spinner.ts
@@ -1,38 +1,41 @@
 /// <mls shortName="spinner" project="100541" enhancement="_100541_enhancementLit" groupName="loader" />
-				
-import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
 
+/**
+ * This code has been forked and modified from a project found on https://github.com/shoelace-style/shoelace.
+ * The original project is licensed under the MIT license. 
+ */
+
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { LocalizeController } from './_100541_internalLocalize';
+
+/**
+ * @summary Spinners are used to show the progress of an indeterminate operation.
+ * @documentation https://shoelace.style/components/spinner
+ * @status stable
+ * @since 2.0
+ *
+ * @csspart base - The component's base wrapper.
+ *
+ * @cssproperty --track-width - The width of the track.
+ * @cssproperty --track-color - The color of the track.
+ * @cssproperty --indicator-color - The color of the spinner's indicator.
+ * @cssproperty --speed - The time it takes for the spinner to complete one animation cycle.
+ */
 @customElement('spinner-100541')
 export class Spinner extends LitElement {
-
-    /** tamanho do spinner
-     * @section principal
-     */
-    @property({ type: Boolean, converter: String })
-    small: 'true' | 'false' = 'false';
 
     createRenderRoot() {
         return this;
     }
+    private readonly localize = new LocalizeController(this);
 
     render() {
-    
-        const smallClass =  this.small === 'true' ? 'a-spinner--small' : ''
         return html`
-        <article class="a-spinner ${smallClass}">
-            <div class="a-spinner_container">
-            <span
-                class="a-spinner_dot a-spinner_dot--1"
-            ></span>
-            <span
-                class="a-spinner_dot a-spinner_dot--2"
-            ></span>
-            <span
-                class="a-spinner_dot a-spinner_dot--3"
-            ></span>
-            </div>
-        </article>
-        `;
+      <svg part="base" class="spinner" role="progressbar" aria-label=${this.localize.term('loading')}>
+        <circle class="spinner__track"></circle>
+        <circle class="spinner__indicator"></circle>
+      </svg>
+    `;
     }
 }
