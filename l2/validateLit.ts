4810f0123d3c265555269f79bcbee0524bd0215f
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-08-09 17:40:47 -0300

    add validateRender

--- l2/validateLit.ts
+++ l2/validateLit.ts
@@ -1,5 +1,5 @@
 /// <mls shortName="validateLit" project="100541" enhancement="_blank" />
-				
+
 import type { IDecoratorDictionary, IDecoratorDetails, IDecoratorClassInfo } from './_100541_propiertiesLit';
 import { convertFileNameToTag } from './_100541_utilsLit';
 
@@ -26,6 +26,15 @@ export function validateTagName(mfile: mls.l2.editor.IMFile) {
             }
         })
     })
+}
+
+export function validateRender(mfile: mls.l2.editor.IMFile) {
+    mfile.storFile.hasError = false;
+    clearErrorsOnModel(mfile.model);
+    if (!mfile || !mfile.compilerResults) return;
+    if (mfile.shortName === 'enhancementLit' && mfile.project === 100541) return;
+    const shortName = `_${mfile.project}_${mfile.shortName}`
+    verify(mfile.model, shortName, mfile)
 }
 
 function getDecoratorClassInfo(decoratorString: string): IDecoratorClassInfo {
@@ -58,3 +67,44 @@ function setErrorOnModel(model: monaco.editor.ITextModel, line: number, startCol
 function clearErrorsOnModel(model: monaco.editor.ITextModel) {
     monaco.editor.setModelMarkers(model, 'markerSource', []);
 }
+
+function verify(model: monaco.editor.ITextModel, shortName: string, mfile: mls.l2.editor.IMFile) {
+    const lines = model.getLinesContent();
+    const tag = convertFileNameToTag(shortName);
+    const msgError = `Do not use the same component tag (${tag}) within the rendering`;
+    let htmlCount: number = 0;
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        line = line.replace(/\/\/.*/, ''); // remove inline comment 
+        const lineInCommentBlock = isInCommentBlock(lines, i + 1);
+        line = line.replace(/\s+/g, ''); // remove blank spaces
+        if (line.indexOf(`document.createElement('${tag}')`) >= 0) {
+            mfile.storFile.hasError = true;
+            setErrorOnModel(model, i + 1, 0, line.length, msgError);
+            break;
+        }
+        if (line.indexOf('html`') >= 0 && !lineInCommentBlock) htmlCount += 1;
+        if (line.indexOf('`') >= 0 && line.indexOf('html`') === -1 && !lineInCommentBlock) htmlCount -= 1;
+
+        if (htmlCount != 0) {
+            if (line.indexOf(tag) >= 0) {
+                mfile.storFile.hasError = true;
+                setErrorOnModel(model, i + 1, 0, line.length, msgError);
+                break;
+            }
+        }
+    }
+}
+
+function isInCommentBlock(lines: string[], lineNumber: number): boolean {
+    let countStartBlockComment = 0;
+    let countEndBlockComment = 0;
+    for (let i = 0; i <= lineNumber - 1; i++) {
+        const line = lines[i];
+        if (line.indexOf('/*') >= 0) countStartBlockComment += 1;
+        if (line.indexOf('*/') >= 0 && i !== lineNumber - 1) countEndBlockComment += 1;
+    }
+    const isInBlockComment = countStartBlockComment > countEndBlockComment;
+    return isInBlockComment;
+};
+
