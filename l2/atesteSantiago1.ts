3cf50504275bf646990ba605ad98592402c07134
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2023-08-10 16:39:46 -0300

    Save mls file:l2/atesteSantiago1.ts

--- l2/atesteSantiago1.ts
+++ l2/atesteSantiago1.ts
@@ -1,3 +1,20 @@
-/// <mls shortName="atesteSantiago1" project="100541" enhancement="_100541_enhancementLit" groupName="midia" />
+/// <mls shortName="atesteSantiago1" project="100541" enhancement="_100541_enhancementBase" groupName="midia" />
 
-// comentario 25 
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js'; 
+ 
+@customElement('ateste-santiago1-100541')
+export class StarRating100541 extends LitElement {
+
+
+  createRenderRoot(){
+    return this;
+  }
+ 
+  render() {
+    
+    return html` 
+        teste 
+    `; 
+  }
+}
